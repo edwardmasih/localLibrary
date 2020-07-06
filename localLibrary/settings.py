81cf247c71638fd684f7f00766b3b17e724bf2cd
Edward Masih <masih.edward45@gmail.com>
2020-07-06 10:25:48 +0530

    added dj_database_url

--- localLibrary/settings.py
+++ localLibrary/settings.py
@@ -138,3 +138,8 @@ django_heroku.settings(locals())
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
