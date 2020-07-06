f38dd8c6ebee1093be9ef277a8cebeeb19dedf40
Edward Masih <masih.edward45@gmail.com>
2020-07-06 10:36:41 +0530

    added staticfiles dir

--- localLibrary/settings.py
+++ localLibrary/settings.py
@@ -139,7 +139,7 @@ django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import dj_database_url
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# # Update database configuration with $DATABASE_URL.
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
